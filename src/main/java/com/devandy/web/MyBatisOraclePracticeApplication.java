7643027429aa245e43923c6e5674e39e2b106806
Youngjin Mo <dev.youngjinmo@gmail.com>
2020-11-06 16:47:06 +0900

    Initialization project

---
+++ src/main/java/com/devandy/web/MyBatisOraclePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.devandy.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBatisOraclePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBatisOraclePracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devandy/web/MyBatisOraclePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devandy.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBatisOraclePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
